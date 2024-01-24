00ff4175190b945f745452c1e2c5ab1d5552664f
zihanzhao0219 <115124427+zihanzhao0219@users.noreply.github.com>
2024-01-24 19:32:42 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsMember() {
+    var skills = ['HTML', 'CSS', 'JS', 'PHP', 'MySQL'];
+    var member = {
+        name: 'John',
+        age: 25,
+        skills: skills
+    };
+    console.log(member.skills[2]);
+}
